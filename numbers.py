17059cc368916d09d3f99ba9d99a066e3ab90d44
safs <safina.samdani@gmail.com>
2021-03-17 01:03:08 +0530

    for complex numbers

--- numbers.py
+++ numbers.py
@@ -3,3 +3,11 @@
 num= 6
 num_sqrt = num ** 0.5
 print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
+
+#square root of real and complex numbers
+
+import cmath
+
+num = 1+2j
+num_sqrt = cmath.sqrt(num)
+print('The square root of {0} is {1:0.3f}+{2:0.3f}j'.format(num ,num_sqrt.real,num_sqrt.imag))
