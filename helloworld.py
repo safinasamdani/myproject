6e64e1146827d2091188fcd40742c85dde714b0c
safs <safina.samdani@gmail.com>
2021-03-16 16:25:12 +0530

    addition and subtraction of two numbers

--- helloworld.py
+++ helloworld.py
@@ -4,6 +4,6 @@ num1= 5
 num2= 10
 sum= num1+num2
 difference = num1-num2
-print "The sum is " , sum
+print ("The sum is " , sum)
 
-print "The difference is ", difference
+print ("The difference is ", difference)
