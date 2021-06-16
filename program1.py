2c83694f3ae993776b6b5783f5bdc82461a9f552
safs <safina.samdani@gmail.com>
2021-06-16 19:22:20 +0530

    tallest

--- program1.py
+++ program1.py
@@ -1,13 +1,48 @@
-a = 5.1
-b = 99.9
-c = 5.8
-if a > b and a > c:
-    tallest = a
-elif b > a and b > c:
-    tallest = b
-else:
-    tallest = c
+
+def tallme():
+    a = input("enter a")
+    a = validateInput(a)
+        
+
+    b = input("enter b")
+    b= validateInput(b)
+
+    c = input("enter c")
+    c= validateInput(c)
     
-print("the tallest is", tallest)
+    tallest = tallMiniMethod(a, b, c)
+
+    print("the tallest is", tallest)
+    return a, b, c
+
+def tallMiniMethod(a, b, c):
+    if a > b and a > c:
+        tallest = a
+    elif b > a and b > c:
+        tallest = b
+    else:
+        tallest = c
+    return tallest
+
+def validateInput(ins):
+    while not ins.isnumeric():
+        print("Please provide a number!!")
+        ins = input("enter number ")
+    return ins
+
+    
+t, s, t = tallme()
+
+
        
 
+def tall(a=3,b=4,c=10):
+
+    tallest = tallMiniMethod(a, b, c)
+    
+    print("the tallest is", tallest)
+
+tall(a=4, b=5, c=11)
+tall()
+tall(15)
+tall(12, 17)
